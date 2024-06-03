acd90035a92cfbca56fae001f658b940321201af
Yadhukrishnx <Yadhukrishnx@gmail.com>
2024-06-03 12:54:46 +0530

    USer authentication : Done

--- baseapp/urls.py
+++ baseapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
    path("base/",views.index,name="home"),
-   path("",views.addtask,name="addtask"),
+   path("addtask",views.addtask,name="addtask"),
    path('search/', views.searchtask, name='searchtask'), 
    path("listtask",views.listtask,name="listtask"),
    path("completedtask",views.completedtask,name="completedtask"),
@@ -11,5 +11,9 @@ urlpatterns = [
    path("completetask/<int:pk>/",views.completetask,name="completetask"),
    path("deletetask/<int:pk>/",views.deletetask,name="deletetask"),
    path("confirmdelete/<int:pk>/",views.confirmdelete,name="confirmdelete"),
+   path("register/",views.register,name="register"),
+   path("",views.login,name="login"),
+   path("logout/",views.logout,name="logout"),
+   
 
 ]
--- baseapp/views.py
+++ baseapp/views.py
@@ -3,8 +3,48 @@ from .models import Task
 from .forms import Taskform
 from django.core.paginator import Paginator,EmptyPage
 from django.db.models import Q
-
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 # Create your views here.
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        cpassword=request.POST.get('cpassword')
+        
+        if password==cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'username already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email already taken')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request,'password not matching')
+            return redirect('register')
+
+    return render(request,'register.html')
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')       
+        password=request.POST.get('password')
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    return render(request,'login.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('register')
 
 def index(request):
     return render(request,'index.html')
